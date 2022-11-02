154bd9617589c539fc218d460f2a37cefe3bfb51
sudo-john-blossom <73914719+sudo-john-blossom@users.noreply.github.com>
2022-11-02 15:41:23 +0000

    sadasd

--- frontend_files/frontend.c
+++ frontend_files/frontend.c
@@ -1,13 +1,18 @@
 #include "frontend.h"
 
-int main(int argc, char* argv){
+int main(int argc, char** argv){
 
     char* user = argv[1];
     char* pass = argv[2];
     ptrClientes client = malloc(sizeof(Clientes));
 
     if(argc < 3){
-        printf("[ERRO] Numero de comandos inseridos invalido");
+        printf("[ERRO] Numero de comandos inseridos invalido\n");
+        printf("[FORMATO] ./frontend username password");
+        return -1;
+    }else if(argc > 3){
+        printf("[ERRO] Numero de comandos inseridos invalido\n");
+        printf("[FORMATO] ./frontend username password");
         return -1;
     }
 
@@ -15,7 +20,7 @@ int main(int argc, char* argv){
         strcpy(client->nome, user);
         strcpy(client->password, pass);
 
-        printf("%s %s", client->nome, client->password); 
+        //printf("%s %s", client->nome, client->password); 
     }
 
     return 0;
--- general.h
+++ general.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define TAM 50
 
